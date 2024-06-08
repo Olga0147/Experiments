1301a47c2c9f00f39814cd3283cb2699af8a7261
olyaz <olyaz@olvin_notebook>
2024-06-08 22:50:02 +0700

    qs

---
+++ ex2/src/main/java/olga0147/ex2/App.java
@@ -0,0 +1,13 @@
+package olga0147.ex2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
